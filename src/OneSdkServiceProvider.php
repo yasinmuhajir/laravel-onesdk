e0d5cf9cc6195929daedb9c76404207e1a4b371c
Denta Zamzam Yasin Muhajir <39980668+yasinmuhajir@users.noreply.github.com>
2018-07-26 16:29:16 +0700

    add service provider and testing (#1)
    * add service provider and testing
    
    * add env and testing
    
    * add env example
    
    * add config and test config
    
    * add factory in provider  and testing
    
    * fix scrutinizer issues

--- src/OneSdkServiceProvider.php
+++ src/OneSdkServiceProvider.php
@@ -1,3 +1,92 @@
 <?php
 
 namespace One\Provider;
+
+use Illuminate\Support\ServiceProvider;
+use One\FactoryArticle;
+use One\FactoryGallery;
+use One\FactoryPhoto;
+use One\FactoryUri;
+use One\Publisher;
+
+class OneSdkServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register Onesdk instance in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerPublisher();
+        $this->registerFactoryUri();
+        $this->registerFactoryArticle();
+        $this->registerFactoryGallery();
+        $this->registerFactoryPhoto();
+    }
+
+    /**
+     * Register Publisher class instance in the container.
+     *
+     * @return void
+     */
+    public function registerPublisher()
+    {
+        $this->app->bind('One\Provider\Publisher', function ($app) {
+            return new Publisher(
+                $app['config']['app.client_id'],
+                $app['config']['app.client_secret'],
+                $app['config']['app.options']
+            );
+        });
+    }
+
+    /**
+     * Register FactoryUri class instance in the container.
+     *
+     * @return void
+     */
+    public function registerFactoryUri()
+    {
+        $this->app->bind('One\Provider\FactoryUri', function () {
+            return new FactoryUri();
+        });
+    }
+
+    /**
+     * Register FactoryArticle class instance in the container.
+     *
+     * @return void
+     */
+    public function registerFactoryArticle()
+    {
+        $this->app->bind('One\Provider\FactoryArticle', function () {
+            return new FactoryArticle();
+        });
+    }
+
+    /**
+     * Register FactoryGallery class instance in the container.
+     *
+     * @return void
+     */
+    public function registerFactoryGallery()
+    {
+        $this->app->bind('One\Provider\FactoryGallery', function () {
+            return new FactoryGallery();
+        });
+    }
+
+    /**
+     * Register FactoryPhoto class instance in the container.
+     *
+     * @return void
+     */
+    public function registerFactoryPhoto()
+    {
+        $this->app->bind('One\Provider\FactoryPhoto', function () {
+            return new FactoryPhoto();
+        });
+    }
+}
---
+++ test/Unit/ServiceProviderTest.php
@@ -0,0 +1,191 @@
+<?php
+namespace One\Provider\Test\Unit;
+
+class ServiceProviderTest extends \Orchestra\Testbench\TestCase
+{
+
+    /**
+     * check whether Publisher class is resolvable in the container
+     *
+     * @test
+     * @return void
+     */
+    public function testPublisherResolvableInContainer()
+    {
+        $publisher = $this->app->make('One\Provider\Publisher');
+        $this->assertInstanceOf('One\Publisher', $publisher);
+    }
+
+    /**
+     * check methods defined in Publisher class
+     *
+     * @test
+     * @return void
+     */
+    public function testMethodPublisherClass()
+    {
+        $publisher = $this->app->make('One\Provider\Publisher');
+        $this->assertTrue(method_exists($publisher, 'recycleToken'));
+        $this->assertTrue(method_exists($publisher, 'assessOptions'));
+        $this->assertTrue(method_exists($publisher, 'requestGate'));
+        $this->assertTrue(method_exists($publisher, 'sendRequest'));
+        $this->assertTrue(method_exists($publisher, 'renewAuthToken'));
+        $this->assertTrue(method_exists($publisher, 'setAuthorizationHeader'));
+        $this->assertTrue(method_exists($publisher, 'getAttachmentEndPoint'));
+        $this->assertTrue(method_exists($publisher, 'getArticleWithIdEndPoint'));
+        $this->assertTrue(method_exists($publisher, 'replaceEndPointId'));
+        $this->assertTrue(method_exists($publisher, 'normalizePayload'));
+        $this->assertTrue(method_exists($publisher, 'submitArticle'));
+    }
+
+    /**
+     * check whether FactoryUri class is resolvable in the container
+     *
+     * @test
+     * @return void
+     */
+
+    public function testFactoryUriResolvableInContainer()
+    {
+        $uriFactory = $this->app->make('One\Provider\FactoryUri');
+        $this->assertInstanceOf('One\FactoryUri', $uriFactory);
+    }
+
+    /**
+     * check methods defined in FactoryUri class
+     *
+     * @test
+     * @return void
+     */
+    public function testMethodFactoryUriClass()
+    {
+        $factoryUri = $this->app->make('One\Provider\FactoryUri');
+        $this->assertTrue(method_exists($factoryUri, 'create'));
+        $this->assertTrue(method_exists($factoryUri, 'createFromString'));
+        $this->assertTrue(method_exists($factoryUri, 'createFromServer'));
+        $this->assertTrue(method_exists($factoryUri, 'createUri'));
+        $this->assertTrue(method_exists($factoryUri, 'validateString'));
+    }
+
+    /**
+     * check whether FactoryArticle class is resolvable in the container
+     *
+     * @test
+     * @return void
+     */
+    public function testFactoryArticleResolvableInContainer()
+    {
+        $articleFactory = $this->app->make('One\Provider\FactoryArticle');
+        $this->assertInstanceOf('One\FactoryArticle', $articleFactory);
+    }
+
+    /**
+     * check methods defined in FactoryArticle class
+     *
+     * @test
+     * @return void
+     */
+    public function testMethodFactoryArticleClass()
+    {
+        $factoryArticle = $this->app->make('One\Provider\FactoryArticle');
+        $this->assertTrue(method_exists($factoryArticle, 'create'));
+        $this->assertTrue(method_exists($factoryArticle, 'checkData'));
+        $this->assertTrue(method_exists($factoryArticle, 'createArticle'));
+        $this->assertTrue(method_exists($factoryArticle, 'validateArray'));
+        $this->assertTrue(method_exists($factoryArticle, 'validateUrl'));
+        $this->assertTrue(method_exists($factoryArticle, 'validateInteger'));
+        $this->assertTrue(method_exists($factoryArticle, 'validateString'));
+    }
+
+    /**
+     * check whether FactoryGallery class is resolvable in the container
+     *
+     * @test
+     * @return void
+     */
+    public function testFactoryGalleryResolvableInContainer()
+    {
+        $galleryFactory = $this->app->make('One\Provider\FactoryGallery');
+        $this->assertInstanceOf('One\FactoryGallery', $galleryFactory);
+    }
+
+    /**
+     * check methods defined in FactoryGallery class
+     *
+     * @test
+     * @return void
+     */
+    public function testMethodFactoryGalleryClass()
+    {
+        $factoryGallery = $this->app->make('One\Provider\FactoryGallery');
+        $this->assertTrue(method_exists($factoryGallery, 'create'));
+        $this->assertTrue(method_exists($factoryGallery, 'checkData'));
+        $this->assertTrue(method_exists($factoryGallery, 'createGallery'));
+        $this->assertTrue(method_exists($factoryGallery, 'validateUrl'));
+        $this->assertTrue(method_exists($factoryGallery, 'validateInteger'));
+        $this->assertTrue(method_exists($factoryGallery, 'validateString'));
+    }
+
+    /**
+     * check whether FactoryPhoto class is resolvable in the container
+     *
+     * @test
+     * @return void
+     */
+
+    public function testFactoryPhotoResolvableInContainer()
+    {
+        $photoFactory = $this->app->make('One\Provider\FactoryPhoto');
+        $this->assertInstanceOf('One\FactoryPhoto', $photoFactory);
+    }
+
+    /**
+     * check methods defined in FactoryPhoto class
+     *
+     * @test
+     * @return void
+     */
+    public function testMethodFactoryPhotoClass()
+    {
+        $factoryPhoto = $this->app->make('One\Provider\FactoryPhoto');
+        $this->assertTrue(method_exists($factoryPhoto, 'create'));
+        $this->assertTrue(method_exists($factoryPhoto, 'checkData'));
+        $this->assertTrue(method_exists($factoryPhoto, 'createPhoto'));
+        $this->assertTrue(method_exists($factoryPhoto, 'validateString'));
+    }
+
+    /**
+     * @test
+     */
+    public function testConfigurationIsLoaded()
+    {
+        $config = $this->app['config']['app'];
+        $this->assertSame($config['client_id'], 0);
+        $this->assertSame($config['client_secret'], 'some-secret-string-token');
+        $this->assertSame($config['options'], array('option1', 'option2'));
+    }
+
+    /**
+     * Load the service provider.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array
+     */
+    protected function getPackageProviders($app)
+    {
+        return ['One\Provider\OneSdkServiceProvider'];
+    }
+
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('app.client_id', 0);
+        $app['config']->set('app.client_secret', 'some-secret-string-token');
+        $app['config']->set('app.options', array('option1', 'option2'));
+    }
+}
